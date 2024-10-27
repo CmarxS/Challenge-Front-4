296dcf2243645c092850af6546ef484ac8b024c8
CmarxS <caiomarques.cmxs@gmail.com>
2024-10-27 17:29:38 -0300

    commit

--- src/app/site/login/page.tsx
+++ src/app/site/login/page.tsx
@@ -1,33 +1,8 @@
 "use client"
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import Formulario from '../../../components/formulario/formulario';
 import Input from '../../../components/input/input';
 
 const Login = () => {
-    const [cpf, setCpf] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const router = useRouter();
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const res = await fetch('/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ cpf, password }),
-        });
-
-        if (res.ok) {
-            router.push('/site/inicial');
-        } else {
-            const data = await res.json();
-            setError(data.message);
-        }
-    };
-
     return (
         <>
             <Formulario
@@ -37,26 +12,20 @@ const Login = () => {
                 link="Crie agora mesmo :)"
                 botao="Acessar conta!"
                 enderecoLink="/site/cadastro"
-                onSubmit={handleSubmit}
             >
                 <Input
                     type="text"
                     name="CPF"
                     placeholder="Digite seu CPF"
                     pattern="\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11}"
-                    value={cpf}
-                    onChange={(e) => setCpf(e.target.value)}
                 />
                 <Input
                     type="password"
                     name="password"
                     placeholder="Digite sua senha"
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
                 />
             </Formulario>
-            {error && <p>{error}</p>}
         </>
     );
 }
